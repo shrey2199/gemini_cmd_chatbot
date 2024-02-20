e13032a8c3705086baa9dffd23c3b9c9f5879cf2
Shreyansh <shreyansh.rawal23@hotmail.com>
2024-02-20 13:54:56 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# gemini_cmd_chatbot" 
---
+++ main.py
@@ -0,0 +1,19 @@
+import google.generativeai as genai
+
+genai.configure(api_key="")
+
+model = genai.GenerativeModel('gemini-pro')
+
+print("Using Text Generative AI Model...")
+
+chat = model.start_chat(history=[])
+
+while True:
+    prompt = input("\nEnter a prompt (Leave Empty to Exit) : ")
+
+    if prompt == "":
+        break
+
+    res = chat.send_message(prompt)
+
+    print(f"\nGemini: {res.text}")
